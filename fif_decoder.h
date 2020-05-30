161ca388818fd0dc7f7b2fea6f89bcbce68ad30b
Extrasklep <extrasklep@firemail.cc>
2020-05-30 22:54:00 +0200

    Move fif utils to fif_decoder.h

--- fif_decoder.h
+++ fif_decoder.h
@@ -22,7 +22,48 @@ struct FIF {
     FIFrgb* decoded_data = nullptr;
 };
 
-#include "utils.h"
+//Utils
+unsigned char* fif_slice;
+char* fif_slicestring;
+//Read bytes of data
+unsigned char* fif_readbytes(FIF* fiffile,unsigned long length) {
+    delete[] fif_slice;
+    fif_slice = new unsigned char[length];
+    for(unsigned int i=0;i<length;i++) {
+        fif_slice[i]=fiffile->data[fiffile->datapos++];
+    }
+    return fif_slice;
+}
+//Get slice as a null terminated string
+char* fif_readbytesAsString(FIF* fiffile,unsigned long length) {
+    unsigned char slicestr[length+1];
+    unsigned char* bytes=fif_readbytes(fiffile,length);
+    for(unsigned int i=0;i<length;i++) {
+        slicestr[i]=bytes[i];
+        if(slicestr[i]=='\0') {slicestr[i]=' ';}
+    }
+    slicestr[length]='\0';
+    delete[] fif_slice;
+    fif_slicestring = new char[length+1];
+    for(unsigned int i=0;i<length+1;i++) {
+        fif_slicestring[i]=slicestr[i];
+    }
+    return fif_slicestring;
+}
+//Get slice and convert it to a number
+unsigned char fif_readbyte(FIF* fiffile) {
+    return fiffile->data[fiffile->datapos++];
+}
+//Spliy byte into bits
+bool fif_bits[8] = {0};
+const bool* fif_getbits(unsigned char b) {
+    unsigned char mask=1;
+    for(unsigned char i=0;i<8;i++) {
+        fif_bits[i]=(b & (mask << i)) != 0;
+    }
+    return fif_bits;
+}
+//-----
 
 const unsigned char fif_bitindex[8] = {0,2,4,1,3,5,6,7};
 
